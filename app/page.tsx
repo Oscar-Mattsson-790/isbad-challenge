b4e3bb88e36ab740fcb643742fbee3a2fe3082e6
Oscar <>
2025-06-23 16:52:00 +0200

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import FeaturesGrid from "@/components/features-grid";
 import { useSupabase } from "@/components/supabase-provider";
