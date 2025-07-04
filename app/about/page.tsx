fcb55ae985458755c4a64aa11516ac7921d7fd63
Oscar <>
2025-07-04 19:35:42 +0200

    Minor styles about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,6 +7,7 @@ import HowItWorks from "@/components/how-it-works";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import LayoutWrapper from "@/components/layout-wrapper";
+import Image from "next/image";
 
 export default function AboutPage() {
   const { supabase } = useSupabase();
@@ -28,29 +29,46 @@ export default function AboutPage() {
 
   return (
     <LayoutWrapper>
-      <div className="max-w-3xl mx-auto space-y-6 pt-10 px-5">
-        <h1 className="text-4xl font-bold">Step Into the Cold – and Grow</h1>
-        <p>
-          ISBAD Challenge is a community-driven movement for those who want to
-          build resilience, discipline, and health through consistent cold
-          exposure. Our app allows you to track your progress, connect with
-          others, and stay motivated on your ice bath journey.
-        </p>
-        <p>
-          Whether you&apos;re a beginner or an experienced cold plunger, our
-          tools help you stay committed and document your experiences. Join us
-          on this transformative journey and let&apos;s build a better future
-          for ourselves and future generations.
-        </p>
+      <div className="max-w-screen-2xl flex flex-col pt-10 px-4 sm:px-0">
+        <div className="flex flex-col lg:flex-row w-full h-full rounded-md overflow-hidden gap-6 items-center">
+          <div className="w-full lg:w-1/2 h-full flex flex-col">
+            <div className="space-y-6">
+              <h1 className="text-4xl font-bold">
+                Step into the Cold – <br /> and Grow
+              </h1>
+              <p>
+                ISBAD Challenge is a community-driven movement for those who
+                want to build resilience, discipline, and health through
+                consistent cold exposure. Our app allows you to track your
+                progress, connect with others, and stay motivated on your ice
+                bath journey.
+              </p>
+              <p>
+                Whether you&apos;re a beginner or an experienced cold plunger,
+                our tools help you stay committed and document your experiences.
+                Join us on this transformative journey and let&apos;s build a
+                better future for ourselves and future generations.
+              </p>
+            </div>
+            <Button
+              onClick={handleStart}
+              className="mt-6 w-full sm:w-auto bg-[#157FBF] border-none hover:bg-[#115F93] hover:text-white"
+              size="lg"
+              disabled={loading}
+            >
+              Get started
+            </Button>
+          </div>
 
-        <Button
-          onClick={handleStart}
-          className="w-full sm:w-auto bg-[#157FBF] border-none hover:bg-[#115F93] hover:text-white"
-          size="lg"
-          disabled={loading}
-        >
-          Get started
-        </Button>
+          <div className="w-full h-[350px] md:h-[500px] lg:w-[650px] lg:h-[650px] rounded-md overflow-hidden relative">
+            <Image
+              src="/images/Man badar i mössa i en isbad Premium med skvätt vatten.jpg"
+              alt="Hero"
+              fill
+              className="object-cover rounded-md"
+            />
+          </div>
+        </div>
 
         <HowItWorks />
 
