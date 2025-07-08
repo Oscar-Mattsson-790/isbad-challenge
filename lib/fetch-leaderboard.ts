9e7302e13c4b9f1769ef217b4dbb207a52bde41a
Oscar <>
2025-07-08 11:05:02 +0200

    fixed friends stats not displaying

--- lib/fetch-leaderboard.ts
+++ lib/fetch-leaderboard.ts
@@ -2,12 +2,12 @@ import { SupabaseClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase";
 
 export async function fetchLeaderboard(supabase: SupabaseClient<Database>) {
-  const { data, error } = await supabase.rpc("get_leaderboard"); // alternativt raw SQL här
+  const { data, error } = await supabase.rpc("get_leaderboard");
 
   if (error) {
     console.error("Error fetching leaderboard:", error.message);
     return [];
   }
-
+  console.log("Leaderboard data:", data);
   return data;
 }
