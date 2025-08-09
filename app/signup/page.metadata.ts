e23f9b218e1308a2a3dbe1b73ddb3c39f8725de0
Oscar <>
2025-08-09 12:45:27 +0200

    seo optimise

--- app/signup/page.metadata.ts
+++ app/signup/page.metadata.ts
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
-
 export const metadata: Metadata = {
   robots: { index: false, follow: false },
-  alternates: { canonical: "https://isbad.com/dashboard" },
+  alternates: { canonical: "https://isbad.com/signup" },
 };
