5b52fc2b67ef9fab8175d6867073b1bd8f43cbf6
Oscar <>
2025-09-29 17:31:38 +0200

    add meta pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import AppLayout from "@/components/layout/app-layout";
 import GAProvider from "./ga-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import Script from "next/script";
 
 import "./globals.css";
 
@@ -62,6 +63,22 @@ export default function RootLayout({
         <Suspense fallback={null}>
           <GAProvider />
         </Suspense>
+
+        {/* ✅ Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '3656279721343267');
+            fbq('track', 'PageView');
+          `}
+        </Script>
 
         {/* Speeed Insights */}
         <SpeedInsights />
